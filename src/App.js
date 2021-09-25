9f73f16f7ebf0cf621781c115c4fb608f30ca6d3
Saeed Setayesh <iamsaeedsetayesh@gmail.com>
2021-09-25 15:47:59 +0330

    fix roting lines

--- src/App.js
+++ src/App.js
@@ -1,94 +1,93 @@
-import { MapContainer, TileLayer, Marker,useMapEvents } from 'react-leaflet'
-import {iconPersonPinPointer} from './icon' 
+import { MapContainer, TileLayer, Marker, useMapEvents } from "react-leaflet";
+import { iconPersonPinPointer } from "./icon";
 import "leaflet/dist/leaflet.css";
+import L from "leaflet";
 
 import Routing from "./Routing";
-import React,{useState} from 'react';
+import React, { useState } from "react";
 
 function App() {
-  const position = [51.505, -0.09];
-  const [postion,setPosition] = useState([51.505, -0.09])
-  const [intervalId,setIntvalId] = useState(null)
-  const [mapFirst,setMapFirest] = useState(false)
-  const [locate,setLoacted] = useState(false)
+  const position = [42.1431055, 24.7523223];
+  const [postion, setPosition] = useState([51.505, -0.09]);
+  const [intervalId, setIntvalId] = useState(null);
+  const [mapFirst, setMapFirest] = useState(false);
+  const [locate, setLoacted] = useState(false);
 
-
-  function  FinderComponent()  {
-        
-    const map = useMapEvents ({
-        layeradd() {
-          map.locate()
-        },
-        locationfound(e) {
-          console.log(e)
-          setPosition(e.latlng)
-          if(!mapFirst){
-          map.flyTo(e.latlng,15)
-          setMapFirest(true)
-          setLoacted(true)
-          }
-        },
-        locationerror(){
-          setPosition({lat: 35.6997, lng: 51.3381})
+  function FinderComponent() {
+    const map = useMapEvents({
+      layeradd() {
+        map.locate();
+      },
+      locationfound(e) {
+        console.log(e);
+        setPosition(e.latlng);
+        if (!mapFirst) {
+          map.flyTo(e.latlng, 15);
+          setMapFirest(true);
+          setLoacted(true);
         }
-      })
-    return (
-        null
-    )
+      },
+      locationerror() {
+        setPosition({ lat: 35.6997, lng: 51.3381 });
+      },
+    });
+    return null;
   }
 
   function FinderComponentEvery() {
-    
     const map = useMapEvents({
       layeradd() {
-        map.locate()
-        let interval  = setInterval(() => {
-          map.locate()
+        map.locate();
+        let interval = setInterval(() => {
+          map.locate();
         }, 10000);
-        setIntvalId(interval)
-        },
-        locationfound(e) {
-          setPosition(e.latlng)
-          setLoacted(true)
-        },
-        locationerror(){
-          setPosition({lat: 35.6997, lng: 51.3381})
-        },
-        
-
-      })
-    return (
-        null
-    )
+        setIntvalId(interval);
+      },
+      locationfound(e) {
+        setPosition(e.latlng);
+        setLoacted(true);
+      },
+      locationerror() {
+        setPosition({ lat: 35.6997, lng: 51.3381 });
+      },
+    });
+    return null;
   }
-
 
   return (
     <div className="App">
       <div className="video-section">
-      <iframe src="https://player.vimeo.com/video/10528962?h=277b919f00&color=f9fc1e" width="100%" height="100%" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
-<p><a href="https://vimeo.com/10528962">Art and Art History</a> from <a href="https://vimeo.com/jesseborkowski">Jesse Borkowski</a> on <a href="https://vimeo.com">Vimeo</a>.</p>
+        <iframe
+          src="https://player.vimeo.com/video/10528962?h=277b919f00&color=f9fc1e"
+          width="100%"
+          height="100%"
+          frameborder="0"
+          allow="autoplay; fullscreen; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+        <p>
+          <a href="https://vimeo.com/10528962">Art and Art History</a> from{" "}
+          <a href="https://vimeo.com/jesseborkowski">Jesse Borkowski</a> on{" "}
+          <a href="https://vimeo.com">Vimeo</a>.
+        </p>
       </div>
       <div className="map-section">
-      <MapContainer center={position} zoom={13} style={{ height: "100vh" }}>
-      <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Routing pos={position} />
-      <FinderComponentEvery/>
-      <FinderComponent />
+        <MapContainer center={position} zoom={16} style={{ height: "100vh" }}>
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Routing pos={position} />
+          <FinderComponentEvery />
+          <FinderComponent />
 
-      <Marker icon={iconPersonPinPointer} position={postion} />
+          <Marker icon={iconPersonPinPointer} position={postion} />
+        </MapContainer>
+      </div>
 
-
-
-    </MapContainer>
-    </div>
-
-    <div className="text-section">
-    <p>past your text here </p>
-    </div>
+      <div className="text-section">
+        <p>past your text here </p>
+      </div>
     </div>
   );
 }
--- src/Routing.js
+++ src/Routing.js
@@ -12,32 +12,83 @@ L.Marker.prototype.options.icon = L.icon({
 export default function Routing({ pos }) {
   const map = useMap();
 
+  var pointA = new L.LatLng(42.1513917, 24.7518429);
+  var pointA1 = new L.LatLng(42.1514719, 24.7520994);
+  var pointA2 = new L.LatLng(42.1512018, 24.7525993);
+  var pointA3 = new L.LatLng(42.1509599, 24.7527699);
+  var pointA4 = new L.LatLng(42.1506462, 24.7527501);
+  var pointA5 = new L.LatLng(42.1501516, 24.7528296);
+  var pointA6 = new L.LatLng(42.1498123, 24.7527169);
+  var pointA7 = new L.LatLng(42.1498821, 24.752088);
+  var pointA8 = new L.LatLng(42.1498543, 24.7517316);
+  var pointA9 = new L.LatLng(42.1493502, 24.7512481);
+  var pointA10 = new L.LatLng(42.1491302, 24.7510335);
+  var pointA11 = new L.LatLng(42.1490071, 24.7507278);
+  var pointA12 = new L.LatLng(42.148504, 24.7508233);
+  var pointA13 = new L.LatLng(42.1464868, 24.7513463);
+  var pointA14 = new L.LatLng(42.1460398, 24.7514965);
+  var pointA15 = new L.LatLng(42.1455053, 24.7514219);
+  var pointA16 = new L.LatLng(42.1459829, 24.7522539);
+  var pointA17 = new L.LatLng(42.1461055, 24.7523223);
+
+  var pointB = new L.LatLng(42.1461055, 24.7523223);
+  var pointList = [
+    pointA,
+    pointA1,
+    pointA2,
+    pointA3,
+    pointA4,
+    pointA5,
+    pointA6,
+    pointA7,
+    pointA8,
+    pointA9,
+    pointA10,
+    pointA11,
+    pointA12,
+    pointA13,
+    pointA14,
+    pointA15,
+    pointA16,
+    pointA17,
+    pointB,
+  ];
+
+  // var firstpolyline = new L.Polyline(pointList, {
+  //   color: "red",
+  //   weight: 3,
+  //   opacity: 0.5,
+  //   smoothFactor: 1,
+  // });
+  // firstpolyline.addTo(map);
+
+  // L.latLng(42.1513917, 24.7518429),
+  // L.latLng(42.1514719, 24.7520994),
+  // L.latLng(42.1512018, 24.7525993),
+  // L.latLng(42.1509599, 24.7527699),
+  // L.latLng(42.1506462, 24.7527501),
+  // L.latLng(42.1501516, 24.7528296),
+  // L.latLng(42.1498123, 24.7527169),
+  // L.latLng(42.1498821, 24.752088),
+  // L.latLng(42.1498543, 24.7517316),
+  // L.latLng(42.1493502, 24.7512481),
+  // L.latLng(42.1491302, 24.7510335),
+  // L.latLng(42.1490071, 24.7507278),
+  // L.latLng(42.148504, 24.7508233),
+  // L.latLng(42.1464868, 24.7513463),
+  // L.latLng(42.1460398, 24.7514965),
+  // L.latLng(42.1455053, 24.7514219),
+  // L.latLng(42.1459829, 24.7522539),
+  // L.latLng(42.1461055, 24.7523223),
+
   useEffect(() => {
     if (!map) return;
 
-    const routingControl = L.Routing.control({
-      waypoints: [
-        L.latLng(42.1513917, 24.7518429),
-        // L.latLng(42.1514719, 24.7520994),
-        // L.latLng(42.1512018, 24.7525993),
-        // L.latLng(42.1509599, 24.7527699),
-        // L.latLng(42.1506462, 24.7527501),
-        // L.latLng(42.1501516, 24.7528296),
-        // L.latLng(42.1498123, 24.7527169),
-        // L.latLng(42.1498821, 24.752088),
-        // L.latLng(42.1498543, 24.7517316),
-        // L.latLng(42.1493502, 24.7512481),
-        // L.latLng(42.1491302, 24.7510335),
-        // L.latLng(42.1490071, 24.7507278),
-        // L.latLng(42.148504, 24.7508233),
-        // L.latLng(42.1464868, 24.7513463),
-        // L.latLng(42.1460398, 24.7514965),
-        L.latLng(42.1455053, 24.7514219),
-        // L.latLng(42.1459829, 24.7522539),
-        // L.latLng(42.1461055, 24.7523223),
-      ],
-      routeWhileDragging: false,
-      addWaypoints: false,
+    const routingControl = new L.Polyline(pointList, {
+      color: "blue",
+      weight: 6,
+      opacity: 0.5,
+      smoothFactor: 1,
     }).addTo(map);
 
     return () => map.removeControl(routingControl);
